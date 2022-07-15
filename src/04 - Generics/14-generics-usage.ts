553406fc9671a3487c4db4336f256483a9419817
Giovane <giovanedaniell@gmail.com>
2022-07-15 13:58:31 -0300

    style: fixed typo

--- src/04 - Generics/14-generics-usage.ts
+++ src/04 - Generics/14-generics-usage.ts
@@ -7,15 +7,15 @@ type AccountMongo = {
 type AccountAPI = Pick<AccountMongo, Exclude<keyof AccountMongo, '_id'>> & { id: string }
 
 /*
-  GHere, we picked from Account Mongo, every key, excluding the _id key, and intersected
+  Here, we picked from Account Mongo, every key, excluding the _id key, and intersected
   the type returned from Pick with the new wanted key id, the result is:
-
-  A good thing from this is: you if you add another key to the AccountMongo type,
-  the AccountAPI type will dinamically have the type too
 
   type AccountAPI = {
     id: string
     name: string
     age: number
   }
+
+  A good thing from this is: you if you add another key to the AccountMongo type,
+  the AccountAPI type will dinamically have the type too
 */
