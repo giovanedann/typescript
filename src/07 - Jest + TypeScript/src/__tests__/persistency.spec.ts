374fbca3a92dc0a9c159d55c92165254f7bd86ed
Giovane <giovanedaniell@gmail.com>
2022-07-28 11:43:31 -0300

    test: test cases for the persistency class

---
+++ src/07 - Jest + TypeScript/src/__tests__/persistency.spec.ts
@@ -0,0 +1,37 @@
+import { Persistency } from '../services/persistency';
+
+const createSut = () => new Persistency();
+
+describe('Test cases for the Persistency class', () => {
+  afterEach(jest.clearAllMocks)
+
+  it('saveOrder method should call console.log (mock with jest.fn)', () => {
+    console.log = jest.fn();
+
+    const sut = createSut(); // sut - system under test
+    sut.saveOrder();
+
+    expect(console.log).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith('Order saved successfully!');
+  });
+
+  it('saveOrder method should call console.log (mock with spyOn)', () => {
+    const logSpy = jest.spyOn(console, 'log');
+
+    const sut = createSut(); // sut - system under test
+    sut.saveOrder();
+
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(logSpy).toHaveBeenCalledWith('Order saved successfully!');
+  })
+
+  it('saveOrder method logs the correct message on the console', () => {
+    const logSpy = jest.spyOn(console, 'log');
+    const sut = createSut(); // sut - system under test
+
+    sut.saveOrder();
+
+    expect(logSpy).toHaveBeenCalledWith('Order saved successfully!');
+  })
+})
