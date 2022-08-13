a9928cb330117c03910ace3acb9f46e5856960a0
Giovane <giovanedaniell@gmail.com>
2022-08-13 11:01:09 -0300

    feat: create Pomodoro component

---
+++ src/08 - React + TypeScript/src/components/Pomodoro/index.tsx
@@ -0,0 +1,24 @@
+import Button from '../Button';
+import Timer from '../Timer';
+import './style.scss';
+
+const Pomodoro: React.FC = () => {
+  return (
+    <div className="pomodoro">
+      <h2>You are working</h2>
+      <Timer
+        defaultTime={3000}
+        shortRestTime={300}
+        longRestTime={900}
+        cycles={4}
+      />
+      <div className="controls">
+        <Button title="Start" onClick={() => alert('eae')} />
+        <Button title="Stop" onClick={() => alert('eae')} />
+        <Button title="Reset" onClick={() => alert('eae')} />
+      </div>
+    </div>
+  );
+};
+
+export default Pomodoro;
