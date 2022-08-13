29ccd78a5c27e2e0f5158288bfa6ce23281c57d8
Giovane <giovanedaniell@gmail.com>
2022-08-13 10:57:24 -0300

    chore(React + Typescript): initial commit - setup done

---
+++ src/08 - React + TypeScript/src/App.tsx
@@ -0,0 +1,11 @@
+import Pomodoro from './components/Pomodoro';
+
+function App() {
+  return (
+    <div className="pomodoro-container">
+      <Pomodoro />
+    </div>
+  );
+}
+
+export default App;
